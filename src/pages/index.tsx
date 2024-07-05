08bfbf7df5341d04bc00b58b80f5d1ba0851d7cd
iescandon <iescan4@gmail.com>
2024-07-05 08:54:21 -0500

    reverted to chapter call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,26 +13,26 @@ const headers = {
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [memberData, setMemberData] = useState<UserData[]>([]);
+  const [memberData, setMemberData] = useState<ChapterUserData[]>([]);
 
   const getMembers = async (cityCode: string) => {
     setIsLoading(true);
-    const response = await axios.get(`/api/members?cityCode=${cityCode}&pageNum=1`);
+    // const response = await axios.get(`/api/members?cityCode=${cityCode}&pageNum=1`);
 
-    let data: UserData[] = response.data.data;
+    // let data: UserData[] = response.data.data;
 
-    const pageArray = Array.from(Array(response.data.last_page + 1).keys()).slice(2, response.data.last_page + 1);
-    const pagePromises = pageArray.map((num) => axios.get(`/api/members?cityCode=${cityCode}&pageNum=${num}`));
-    const pageResponses = await Promise.all(pagePromises);
+    // const pageArray = Array.from(Array(response.data.last_page + 1).keys()).slice(2, response.data.last_page + 1);
+    // const pagePromises = pageArray.map((num) => axios.get(`/api/members?cityCode=${cityCode}&pageNum=${num}`));
+    // const pageResponses = await Promise.all(pagePromises);
 
-    pageResponses.forEach((response: any) => {
-      data = data.concat(response.data.data);
-    })
+    // pageResponses.forEach((response: any) => {
+    //   data = data.concat(response.data.data);
+    // })
 
-    setMemberData(data)
-    // const response = await axios.get(`/api/chapters?cityCode=${cityCode}`);
-    // console.log(Object.values(response.data.latinas_chapters))
-    // setMemberData(Object.values(response.data.latinas_chapters))
+    // setMemberData(data)
+    const response = await axios.get(`/api/chapters?cityCode=${cityCode}`);
+    console.log(Object.values(response.data.latinas_chapters))
+    setMemberData(Object.values(response.data.latinas_chapters))
     setIsLoading(false);
   }
 
